ec6efe7bdf2747ea93d674f6b9b501667ec33996
Doro Hassan <dorothea.hassan@gmail.com>
2019-09-17 17:23:23 +0200

    Iteration 1 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import fullListOfContacts from './contacts.json'
 import './App.css';
 
 function App() {
+  // The initial state hook `contacts` is initialised with the 5 first elements of fullListOfContacts
+  const [contacts, setContacts] = useState(fullListOfContacts.slice(0,5));
+  // ...
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+      </thead>
+
+      <tbody>
+          {contacts.map((contact, i) => (
+            <tr key={i}>
+              <td>
+                <img src={contact.pictureUrl} style={{width: 100}} alt="pictureUrl" />
+              </td>
+              <td>
+                {contact.name}
+              </td>
+              <td>
+                {contact.popularity}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      
+
     </div>
   );
 }
